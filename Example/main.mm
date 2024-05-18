dad6314aef6c7c1bd0fe9c5f93fe23f4639333ea
Dave <dave@toaster.llc>
2024-05-17 20:08:59 -0700

    LMMSE progress

--- Example/main.mm
+++ Example/main.mm
@@ -8,6 +8,10 @@
 #import <chrono>
 namespace fs = std::filesystem;
 
+static bool _IsCFAFile(const fs::path& path) {
+    return fs::is_regular_file(path) && path.extension() == ".cfa";
+}
+
 static bool _IsPNGFile(const fs::path& path) {
     return fs::is_regular_file(path) && path.extension() == ".png";
 }
@@ -15,19 +19,83 @@ static bool _IsPNGFile(const fs::path& path) {
 int main(int argc, const char* argv[]) {
     using namespace std::chrono;
     
+    static constexpr Toastbox::CFADesc CFADesc = {
+        Toastbox::CFAColor::Green, Toastbox::CFAColor::Red,
+        Toastbox::CFAColor::Blue, Toastbox::CFAColor::Green,
+    };
+    
     const fs::path imagesDir = fs::path(argv[0]).parent_path() / "Images";
+    
+    
+    
+    
+    
+    
+    
+    
     
     id<MTLDevice> device = MTLCreateSystemDefaultDevice();
     Toastbox::Renderer renderer(device, [device newDefaultLibrary], [device newCommandQueue]);
+    
+    
+    
+    
+    for (const fs::path& p : fs::directory_iterator("/Users/dave/repos/LMMSE-Metal/Example/Images")) {
+        if (_IsCFAFile(p)) {
+            const Toastbox::Mmap mmap(p);
+            const uint16_t* rawImageData = (uint16_t*)(mmap.data()+32);
+            constexpr size_t ThumbWidth = 576;
+            constexpr size_t ThumbHeight = 324;
+//            const size_t rawImageDataLen = ThumbWidth * ThumbHeight * 2; // 373248 bytes
+            
+            Toastbox::Renderer::Txt txtRaw = renderer.textureCreate(MTLPixelFormatR16Float, ThumbWidth, ThumbHeight);
+            renderer.textureWrite(txtRaw, rawImageData, 1);
+            
+            
+            
+            
+            
+            constexpr MTLTextureUsage TxtRgbUsage =
+                MTLTextureUsageShaderRead   |
+                MTLTextureUsageShaderWrite  |
+                MTLTextureUsageRenderTarget ;
+            
+            Toastbox::Renderer::Txt txtRgb = renderer.textureCreate(MTLPixelFormatRGBA32Float,
+                [txtRaw width], [txtRaw height], TxtRgbUsage);
+            
+            LMMSE::Run(renderer, CFADesc, true, txtRaw, txtRgb);
+            
+            renderer.debugTextureShow(txtRgb);
+            
+            
+            
+            
+            
+            
+//            printf("path: %s\n", p.c_str());
+//            
+//            auto illuminant = FFCCModel::Run(renderer, txt);
+//            printf("illuminant: %f %f %f\n", illuminant[0], illuminant[1], illuminant[2]);
+//            
+//            renderer.commitAndWait();
+//            renderer.debugTextureWrite(txt, fs::path(p.string()+".png"));
+            
+//            renderer.debugTextureShow(txt);
+//            break;
+//            576 * 324 = 186624 pixels * 2 = 373248 bytes
+//            printf("%s\n", p.string().c_str());
+        }
+    }
+    
+    
+    
+    
+    
+    
     MTKTextureLoader* txtLoader = [[MTKTextureLoader alloc] initWithDevice:device];
     std::vector<id<MTLTexture>> txts;
     for (const fs::path& p : fs::directory_iterator(imagesDir)) @autoreleasepool {
         if (_IsPNGFile(p)) {
-            static constexpr Toastbox::CFADesc CFADesc = {
-                Toastbox::CFAColor::Green, Toastbox::CFAColor::Red,
-                Toastbox::CFAColor::Blue, Toastbox::CFAColor::Green,
-            };
-            
             id<MTLTexture> txtRaw = [txtLoader newTextureWithContentsOfURL:[NSURL fileURLWithPath:@(p.c_str())]
                 options:nil error:nil];
             txts.push_back(txtRaw);
